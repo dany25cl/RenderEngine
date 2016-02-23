a76b666b1e5fc54ba9fb1e5f72697aa1555234f1
dany25cl <daniel_abad1@hotmail.com>
2016-02-23 15:52:58 +0100

    start

---
+++ Render_Engine/src/camera.h
@@ -0,0 +1,18 @@
+#ifndef CAMERA_H_
+#define CAMERA_H_
+
+#include <glm/glm.hpp>
+
+class Camera{
+	glm::mat4 view;	
+	glm::mat4 proj;	
+public:
+	Camera();
+	~Camera();
+	void setPos(const glm::vec4 &pos);
+	glm::vec4 getPos();
+	inline glm::mat4 getView(){ return view; }
+
+
+};
+#endif
